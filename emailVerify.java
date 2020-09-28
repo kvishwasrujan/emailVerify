bf93b76e6d296355a6f97248cd14ca76487187ac
kvishwasrujan <roohithbandi00@gmail.com>
2020-09-28 19:29:11 +0530

    Added email Verification

---
+++ emailVerify.java
@@ -0,0 +1,16 @@
+package regexJava;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class regexUC9 {
+	public static void main(String[] args) {
+		// main method
+		// condition for Email
+		final String Email = "^abc.{4}[a-z]{3}@bl.co.{7}[a-z]{2}$";
+		final String INPUT = "abc.xyz@bl.co.ww";
+		Pattern pattern = Pattern.compile(Email);
+		Matcher matcher = pattern.matcher(INPUT);
+		System.out.println("Input String matches regex-" + matcher.matches());
+	}
+}
